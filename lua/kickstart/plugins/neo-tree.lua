598a0cc677a5979bcb4a708c335ae779849602a6
frankhaotic <91132169+ffloodRI@users.noreply.github.com>
2024-04-22 12:34:50 +0100

    Update neo-tree.lua mappings

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -8,13 +8,17 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
+    -- '3rd/image.nvim', -- optional image support in preview window
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+    { '<leader>e', ':Neotree toggle<CR>', { desc = 'NeoTree toggle' } },
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        hide-dotfiles = false,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
